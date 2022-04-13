0fa300d82a869dfca31d6ce6ef38c1bce1e412e9
mvsg2 <89340753+mvsg2@users.noreply.github.com>
2022-04-13 14:08:36 +0530

    Create Pascal_Triangle.py
    This program takes the number of rows to be printed as user input.

---
+++ Pascal_Triangle.py
@@ -0,0 +1,14 @@
+def printPascal(n):
+  n = int(input("How many rows of the Pascal's triangle do you want to print? \n"))
+  for i in range(1, n+1):
+    for j in range(0, n-i+1):
+      print(' ', end = ' ')
+    
+    C = 1
+    for j in range(1, i + 1):
+      print(' ', C, sep=' ', end=' ')
+      
+      C = C * (i -j) // j
+    print()
+    
+# printPascal(6)
