206c26df09fbb1e20b30f0fed8220fa4284126d6
Sai Ganesh Manda <89340753+mvsg2@users.noreply.github.com>
2023-01-10 16:39:47 +0530

    Update YouTube_Playlist_Downloader.py

--- YouTube_Playlist_Downloader.py
+++ YouTube_Playlist_Downloader.py
@@ -1,34 +1,5 @@
 from pytube import Playlist
 
-# playlist = Playlist(url='https://www.youtube.com/playlist?list=PLWcODnEVd5usT6sbQsQ9lnffLGHBCrcEH')
-# print()
-# print("Playlist title:", playlist.title)
-
-
-# print(playlist.description)
-# print(playlist.thumbnail)
-# print(playlist.author)
-# print(playlist.views)
-# print(playlist.length) # the same as print(len(playlist.video_urls))
-# print(playlist.views_count)
-# print(playlist.likes_count)
-
-
-# donwload_folder = 'E:/YouTube_Downloads/Walt Disney/'
-# print("Number of videos:", len(playlist.video_urls))
-# print("Downloading the playist...\n")
-# video_count = 0
-# for i, video in zip(range(len(playlist.video_urls)), playlist.videos):
-#     print(i+1, end=' ')
-#     print("Downloading -- {}".format(video.title))
-#     video.streams.get_highest_resolution().download(output_path=donwload_folder)
-#     print("Downloaded the video - {}".format(video.title))
-#     video_count += 1
-# if (video_count == len(playlist.video_urls)):
-#     print("\nDownloaded all videos of the playlist!")
-# else:
-#     print("Downloaded {} videos of the playlist".format(video_count))
-
 def YouTubePlaylistDownloader(playlist_url, download_path):
 
     playlist = Playlist(url=playlist_url)
